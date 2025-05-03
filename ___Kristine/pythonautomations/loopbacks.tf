9c1556f00f86180d69a3163ff735314ca43c7989
Kristine1204 <kristineyarra1204@gmail.com>
2025-05-03 12:55:33 +0800

    Add files via upload

---
+++ ___Kristine/pythonautomations/loopbacks.tf
@@ -0,0 +1,15 @@
+
+resource "cisco_loopback" "lo1" {
+  name = "Loopback1"
+  ip   = "1.1.1.1"
+}
+
+resource "cisco_loopback" "lo2" {
+  name = "Loopback2"
+  ip   = "2.2.2.2"
+}
+
+resource "cisco_loopback" "lo3" {
+  name = "Loopback3"
+  ip   = "3.3.3.3"
+}
